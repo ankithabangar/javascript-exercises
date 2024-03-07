cad148cbc01cd4bee266874ad48f33af9e567d93
Ankitha Bangar <ankitha.bangar@st.ovgu.de>
2024-03-07 11:44:10 +0100

    Completed 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elements) {
+  for (let element of elements) {
+    let index = array.indexOf(element);
+    while (index !== -1) {
+      array.splice(index, 1);
+      index = array.indexOf(element);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+function isInvalidNumber(element) {
+  return element < 0 || typeof element !== "number";
+}
 
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  let min = num1;
+  let max = num2;
+  if (isInvalidNumber(num1) || isInvalidNumber(num2)) {
+    return "ERROR";
+  }
+  if (num2 < num1) {
+    min = num2;
+    max = num1;
+  }
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
