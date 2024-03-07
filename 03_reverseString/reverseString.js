31a501e4ca68ed92f56cd88acc4deff4ef44a4cb
Ankitha Bangar <ankitha.bangar@st.ovgu.de>
2024-03-07 08:38:09 +0100

    Completed 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let splitString = string.split("");
+  let reverseArray = splitString.reverse().join("");
+  return reverseArray;
 };
 
 // Do not edit below this line
