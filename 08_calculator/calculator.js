ed5bbfede9a9f13231af678632cef4412675be4a
Ankitha Bangar <ankitha.bangar@st.ovgu.de>
2024-03-21 14:04:56 +0100

    Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (input, ...inputs) {
+  let sum = 0;
+  sum = input + parseInt(inputs);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (input, ...inputs) {
+  let minus = 0;
+  minus = input - inputs;
+  return minus;
 };
 
-const sum = function() {
-	
+const sum = function (inputs) {
+  let add = inputs.reduce((accumulator, ele) => accumulator + ele, 0);
+  return add;
 };
 
-const multiply = function() {
-
+const multiply = function (inputs) {
+  let multi = inputs.reduce(
+    (accumulator, currentItem) => accumulator * currentItem
+  );
+  return multi;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let powerOf = num1 ** num2;
+  return powerOf;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let facto = 1;
+  while (num > 1) {
+    facto *= num;
+    num--;
+  }
+  return facto;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
